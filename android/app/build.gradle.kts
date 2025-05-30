b6227f66a8b085f99c449ed35f1d43b833626c38
Chengran Sun <csun10@lakeheadu.ca>
2025-05-30 11:26:38 -0400

    update: pubspec.yaml & build.gradle.kts
    1. add firebase_core in pubspec.yaml
    2. update ndkVersion in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,9 +9,9 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"
     namespace = "com.example.mastermind_game"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
